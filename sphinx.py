91b9b2601a7d9cb9b4832bfb74d3033da69598c0
stf <7o5rfu92t@ctrlc.hu>
2018-01-31 19:10:33 +0100

    [mod] renamed py client

--- sphinx.py
+++ sphinx.py
@@ -1,163 +1,69 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
+"""
+Wrapper for libsphinx library
 
-import pysodium, sys, os, asyncio, subprocess, io, struct, binascii
-import bin2pass
+    Copyright (c) 2018, Marsiske Stefan.
+    All rights reserved.
 
-verbose = False
-addr = '127.0.0.1'
-port = 2355
-datadir = '~/.sphinx/'
+    This file is part of pitchforked sphinx.
 
-CREATE=b'\x00'
-GET=b'\x66'
-CHANGE=b'\xaa'
-DELETE=b'\xff'
+    pitchforked sphinx is free software: you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public License
+    as published by the Free Software Foundation, either version 3 of
+    the License, or (at your option) any later version.
 
-class SphinxClientProtocol(asyncio.Protocol):
-  def __init__(self, message, loop,b):
-    self.b = b
-    self.message = message
-    self.loop = loop
+    pitchfork is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-  def connection_made(self, transport):
-    transport.write(self.message)
-    if verbose: print('Data sent: {!r}'.format(self.message))
+    You should have received a copy of the GNU General Public License
+    along with pitchforked sphinx. If not, see <http://www.gnu.org/licenses/>.
+"""
 
-  def data_received(self, data):
-    if verbose: print('Data received: {!r}'.format(data.decode()))
-    if data == b'fail':
-        print('fail')
-        return
+import ctypes
+import ctypes.util
 
-    if not self.b:
-        return
+sphinxlib = ctypes.cdll.LoadLibrary(ctypes.util.find_library('sphinx') or ctypes.util.find_library('libsphinx'))
 
-    rwd=finish(self.b, data)
+if not sphinxlib._name:
+    raise ValueError('Unable to find libsphinx')
 
-    rule = getrule(datadir, id)
-    if not rule:
-        print("no password rule defined for this password.")
-    rule, size = rule
-    print(bin2pass.derive(rwd,rule,size).decode())
+DECAF_255_SCALAR_BYTES = 32
+DECAF_255_SER_BYTES    = 32
 
-  def connection_lost(self, exc):
-    if verbose:
-        print('The server closed the connection')
-        print('Stop the event loop')
-    self.loop.stop()
+def __check(code):
+    if code != 0:
+        raise ValueError
 
-def getkey(datadir):
-  datadir = os.path.expanduser(datadir)
-  try:
-    fd = open(datadir+'key', 'rb')
-    key = fd.read()
-    fd.close()
-    return key
-  except FileNotFoundError:
-    if not os.path.exists(datadir):
-      os.mkdir(datadir,0o700)
-    pk, sk = pysodium.crypto_sign_keypair()
-    with open(datadir+'key','wb') as fd:
-      os.fchmod(fd.fileno(),0o600)
-      fd.write(sk)
-    return sk
+# void challenge(const uint8_t *pwd, const size_t p_len, uint8_t *bfac, uint8_t *chal)
+def challenge(pwd):
+    if pwd == None:
+        raise ValueError("invalid parameter")
+    bfac = ctypes.create_string_buffer(DECAF_255_SCALAR_BYTES)
+    chal = ctypes.create_string_buffer(DECAF_255_SER_BYTES)
+    __check(sphinxlib.challenge(pwd, len(pwd), bfac, chal))
+    return (bfac.raw, chal.raw)
 
-def getsalt(datadir):
-  datadir = os.path.expanduser(datadir)
-  try:
-    fd = open(datadir+'salt', 'rb')
-    salt = fd.read()
-    fd.close()
-    return salt
-  except FileNotFoundError:
-    salt = pysodium.randombytes(32)
-    with open(datadir+'salt','wb') as fd:
-      os.fchmod(fd.fileno(),0o600)
-      fd.write(salt)
-    return salt
+# int respond(const uint8_t *chal, const uint8_t *secret, uint8_t *resp)
+def respond(chal, secret):
+    if None in (chal, secret):
+        raise ValueError("invalid parameter")
+    if len(chal) != DECAF_255_SER_BYTES: raise ValueError("truncated point")
+    if len(secret) != DECAF_255_SCALAR_BYTES: raise ValueError("truncated secret")
 
-def saverules(datadir, id, rules, size):
-  datadir = os.path.expanduser(datadir+'/rules/')
-  if not os.path.exists(datadir):
-      os.mkdir(datadir,0o700)
-  # convert rule to bitfields
-  rules = sum(1<<i for i, c in enumerate(('u','l','s','d')) if c in rules)
-  # pack rule
-  rule=(rules << 7) | (size & 0x7f)
-  with open(datadir+binascii.hexlify(id).decode(), 'wb') as fd:
-      fd.write(struct.pack('>H', rule))
+    resp = ctypes.create_string_buffer(DECAF_255_SER_BYTES)
 
-def getrule(datadir, id):
-  datadir = os.path.expanduser(datadir+'/rules/')
-  try:
-    fd = open(datadir+binascii.hexlify(id).decode(), 'rb')
-    rule = struct.unpack(">H",fd.read(2))[0]
-    size = (rule & 0x7f)
-    rule = {c for i,c in enumerate(('u','l','s','d')) if (rule >> 7) & (1 << i)}
-    return (rule, size)
-  except FileNotFoundError:
-      return None
+    __check(sphinxlib.respond(chal, secret, resp))
+    return resp.raw
 
-def usage():
-  print("usage: %s <create> <user> <site> [u][l][d][s] [<size>]" % sys.argv[0])
-  print("usage: %s <get|change|delete> <user> <site>" % sys.argv[0])
-  sys.exit(1)
+# int finish(const uint8_t *bfac, const uint8_t *resp, uint8_t *rwd)
+def finish(bfac, resp):
+    if None in (bfac, resp):
+        raise ValueError("invalid parameter")
+    if len(resp) != DECAF_255_SER_BYTES: raise ValueError("truncated point")
+    if len(bfac) != DECAF_255_SCALAR_BYTES: raise ValueError("truncated secret")
 
-def challenge():
-  res=subprocess.run(['./challenge'], stdin=sys.stdin, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-  res.check_returncode()
-  return res.stderr, res.stdout
-
-def finish(b, resp):
-  res=subprocess.run(['./derive', b], input=resp, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-  res.check_returncode()
-  return res.stdout
-
-if __name__ == '__main__':
-  if ((len(sys.argv) > 1 and sys.argv[1]!='create' and len(sys.argv) != 4) or
-      (len(sys.argv) > 1 and sys.argv[1]=='create' and len(sys.argv)!=6)):
-      usage()
-
-  sk = getkey(datadir)
-  salt = getsalt(datadir)
-  id = pysodium.crypto_generichash(''.join((sys.argv[2],sys.argv[3])), salt, 32)
-  b = None
-
-  if sys.argv[1] == 'create':
-    # needs pubkey, id, challenge, sig(id)
-    # returns output from ./response | fail
-    if set(sys.argv[4]) - {'u','l','s','d'}:
-        print("error: rules can only contain ulsd.")
-        usage()
-    size = 0
-    try: size=int(sys.argv[5])
-    except:
-        print("error: size has to be integer.")
-        usage()
-    saverules(datadir, id, sys.argv[4], size)
-    b, c = challenge()
-    message = [CREATE,id,c, pysodium.crypto_sign_sk_to_pk(sk)]
-  elif sys.argv[1] == 'get':
-    # needs id, challenge, sig(id)
-    b, c = challenge()
-    message = [GET,id,c]
-    # returns output from ./response | fail
-  elif sys.argv[1] == 'change':
-    # needs id, challenge, sig(id)
-    b, c = challenge()
-    message = [CHANGE,id,c]
-    # changes stored secret
-    # returns output from ./response | fail
-  elif sys.argv[1] == 'delete':
-    # needs id, sig(id)
-    message = [DELETE,id]
-  else:
-    usage()
-
-  message=pysodium.crypto_sign(b''.join(message),sk)
-
-  loop = asyncio.get_event_loop()
-  coro = loop.create_connection(lambda: SphinxClientProtocol(message, loop, b), addr, port)
-  loop.run_until_complete(coro)
-  loop.run_forever()
-  loop.close()
+    rwd = ctypes.create_string_buffer(DECAF_255_SER_BYTES)
+    __check(sphinxlib.finish(bfac, resp, rwd))
+    return rwd.raw
