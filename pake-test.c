1fae48aec6b141947536908c70c39b53cb30c5d7
stf <7o5rfu92t@ctrlc.hu>
2018-02-01 23:00:49 +0100

    [mod] added license header

--- pake-test.c
+++ pake-test.c
@@ -1,3 +1,21 @@
+/*
+    @copyright 2018, pitchfork@ctrlc.hu
+    This file is part of pitchforked sphinx.
+
+    pitchforked sphinx is free software: you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public License
+    as published by the Free Software Foundation, either version 3 of
+    the License, or (at your option) any later version.
+
+    pitchfork is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with pitchforked sphinx. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include "pake.h"
