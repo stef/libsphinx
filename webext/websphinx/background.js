471decc51a0524d4bf5b0b01a287d00d0771f63a
stf <7o5rfu92t@ctrlc.hu>
2018-02-24 18:37:59 +0100

    [mod] chrom/bg.js holds state for manual inserts, some restructoring of the code

--- webext/websphinx/background.js
+++ webext/websphinx/background.js
@@ -26,28 +26,69 @@ var portFromCS;
 var nativeport = browser.runtime.connectNative(APP_NAME);
 var changeData = true;
 
+var state = {
+  user: '',
+  site: '',
+  login: '',
+  change: '',
+  create: ''
+};
+
+function reset_state() {
+  state.user='';
+  state.site='';
+  state.change='';
+  state.create='';
+  state.login='';
+}
+
+function set_state(site, user, cmd, pwd) {
+  if(site != state.site || user!=state.user) reset_state();
+  state.site=site;
+  state.user=user;
+  if(cmd=="create") state.create=pwd;
+  if(cmd=="change") state.change=pwd;
+  if(cmd=="login") state.login=pwd;
+}
+
 nativeport.onMessage.addListener((response) => {
+  // internal error handling
   if (browser.runtime.lastError) {
     var error = browser.runtime.lastError.message;
     console.error(error);
     portFromCS.postMessage({ status: "ERROR", error: error });
-  } else if(response.results == 'fail') {
+    return;
+  }
+
+  // client error handling
+  if(response.results == 'fail') {
     console.log('websphinx failed');
-  } else if(response.results.mode == "manual") {
+    return;
+  }
+
+  // cache password for manual inserts
+  if(response.results.cmd=="change" || response.results.cmd=="create" || response.results.cmd=="login") {
+    set_state(response.results.site, response.results.name, response.results.cmd, response.results.password);
+  }
+
+  // handle manual inserts
+  if(response.results.mode == "manual") {
     //console.log("manual");
-    // its a manual mode response so we just pass it to the popup
-    portFromCS.postMessage(response);
-  } else if(response.results.cmd == 'show') {
+    // its a manual mode response so we just insert it via inject
+    browser.tabs.executeScript({code: 'document.websphinx.inject(' + JSON.stringify(response.results.password) + ');'});
+    return;
+  }
+
+  // handle get current pwd
+  if(response.results.cmd == 'login') {
+    // 1st step in an automatic change pwd
     if(changeData==true) {
       // we got the old password
-      changeData=response.results;
+      set_state(response.results.site, response.results.name, response.results.cmd, response.results.password);
       // now change the password
-      nativeport.postMessage({
-        cmd: "change",
-        mode: response.results.mode,
-        site: response.results.site,
-        name: response.results.name
-      });
+      response.results.cmd="change";
+      delete response.results['password']; // don't send the password back
+      nativeport.postMessage(response.results);
       return;
     }
     let login = {
@@ -55,24 +96,47 @@ nativeport.onMessage.addListener((response) => {
       password: response.results.password
     };
     browser.tabs.executeScript({code: 'document.websphinx.login(' + JSON.stringify(login) + ');'});
-  } else if(response.results.cmd == 'list') {
+    return;
+  }
+
+  // handle list users
+  if(response.results.cmd == 'list') {
     portFromCS.postMessage(response);
-  } else if(response.results.cmd == 'create') {
+    return;
+  }
+
+  // handle create password
+  if(response.results.cmd == 'create') {
     let account = {
       username: response.results.name,
       password: response.results.password
     };
     browser.tabs.executeScript({code: 'document.websphinx.create(' + JSON.stringify(account) + ');'});
-  } else if(response.results.cmd == 'change') {
+    return;
+  }
+
+  // handle change password
+  if(response.results.cmd == 'change') {
+    if(state.site!=response.results.site || state.user!=response.results.name) {
+      reset_state();
+      return; // todo signal error?
+    }
     let change = {
-      'old': changeData,
-      'new': response.results
+      'old': state.login,
+      'new': response.results.password
     }
     browser.tabs.executeScript({code: 'document.websphinx.change(' + JSON.stringify(change) + ');'});
     changeData = false;
-  } else if(response.results.cmd == 'commit') {
-    portFromCS.postMessage(response);
+    return;
   }
+
+  // handle commit result
+  if(response.results.cmd == 'commit') {
+    portFromCS.postMessage(response);
+    return;
+  }
+  console.log("unhandled native port response");
+  console.log(response);
 });
 
 browser.runtime.onConnect.addListener(function(p) {
@@ -80,58 +144,52 @@ browser.runtime.onConnect.addListener(function(p) {
 
   // proxy from CS to native backend
   portFromCS.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action == "login") {
-      //console.log("login");
-      changeData=false;
-      nativeport.postMessage({
-        cmd: "show",
-        mode: request.mode,
-        site: request.site,
-        name: request.name});
-    } else if (request.action == "list") {
-      //console.log("list");
-      nativeport.postMessage({
+    // clear state if the site has changed
+    if(request.site!=state.site) reset_state();
+    // same site, if manual insert do so
+    else if(request.mode == "manual" &&
+            (request.action == 'change' || request.action == 'login' || request.action == 'create') &&
+            state[request.action] != '') {
+        if(request.name!=state.user) reset_state();
+        else {
+          browser.tabs.executeScript({code: 'document.websphinx.inject(' + JSON.stringify(state[request.action]) + ');'});
+          return;
+        }
+    }
+
+    // prepare message to native backend
+    let msg = {
         cmd: request.action,
         mode: request.mode,
-        site: request.site});
-    } else if (request.action == "create") {
-      //console.log("create");
-      nativeport.postMessage({
-        cmd: request.action,
-        mode: request.mode,
-        site: request.site,
-        name: request.name,
-        rules: request.rules,
-        size: request.size
-      });
-    } else if (request.action == "change") {
-      //console.log("change");
-      if(request.mode == "manual") {
-        nativeport.postMessage({
-          cmd: "change",
-          mode: request.mode,
-          site: request.site,
-          name: request.name
-        });
-      } else {
+        site: request.site
+    };
+
+    if(request.action!="list") msg.name=request.name;
+    if (request.action == "login") changeData=false;
+    if (request.action == "create") {
+      msg.rules= request.rules;
+      msg.size= request.size;
+    }
+    if (request.action == "change") {
+      if(request.mode != "manual") {
         // first get old password
         // but this will trigger the login inject in the nativport onmessage cb
         changeData = true;
-        nativeport.postMessage({
-          cmd: "show",
-          mode: request.mode,
-          site: request.site,
-          name: request.name
-        });
+        msg.cmd= "login";
       }
-    } else if (request.action == "commit") {
-      //console.log("commit");
-      nativeport.postMessage({
-        cmd: request.action,
-        mode: request.mode,
-        site: request.site,
-        name: request.name
-      });
     }
+
+    if(request.action!="login" &&
+       request.action!="list" &&
+       request.action!="create" &&
+       request.action!="change" &&
+       request.action!="commit") {
+      console.log("unhandled popup request");
+      console.log(request);
+      return;
+    }
+
+    // send request to native backend
+    nativeport.postMessage(msg);
   });
 });
