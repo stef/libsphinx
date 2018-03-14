3bbf1e878d0af950bdb2d32a0479e59fe0efbf52
stf <7o5rfu92t@ctrlc.hu>
2018-03-14 14:36:13 +0100

    [mod] made sphinx.h independent of decaf.h

--- src/sphinx.h
+++ src/sphinx.h
@@ -3,7 +3,9 @@
 
 #include <stdint.h>
 #include <stdlib.h>
-#include "decaf.h"
+
+#define SPHINX_255_SCALAR_BYTES 32
+#define SPHINX_255_SER_BYTES 32
 
 void challenge(const uint8_t *pwd, const size_t p_len, uint8_t *bfac, uint8_t *chal);
 int respond(const uint8_t *chal, const uint8_t *secret, uint8_t *resp);
