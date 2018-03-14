4238b40a65a7964f781e593301d0f3eae7e61312
stf <7o5rfu92t@ctrlc.hu>
2018-03-14 14:36:28 +0100

    [mod] adopted test/test.c to new sphinx.h

--- src/tests/test.c
+++ src/tests/test.c
@@ -4,11 +4,11 @@
 
 int main(void) {
   const uint8_t pwd[]="shitty password";
-  const uint8_t secret[DECAF_255_SCALAR_BYTES]="                                ";
-  uint8_t bfac[DECAF_255_SCALAR_BYTES],
-    chal[DECAF_255_SER_BYTES],
-    resp[DECAF_255_SER_BYTES],
-    rwd[DECAF_255_SER_BYTES];
+  const uint8_t secret[SPHINX_255_SCALAR_BYTES]="                                ";
+  uint8_t bfac[SPHINX_255_SCALAR_BYTES],
+    chal[SPHINX_255_SER_BYTES],
+    resp[SPHINX_255_SER_BYTES],
+    rwd[SPHINX_255_SER_BYTES];
 
   challenge(pwd, sizeof pwd, bfac, chal);
   if(0!=respond(chal, secret, resp)) {
