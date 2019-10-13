b4876007fcf79ad5146abfe96203cea6f6dcf2e3
stf <7o5rfu92t@ctrlc.hu>
2019-10-13 13:50:58 +0000

    [enh] adapted sphinx test to dedecaffed sphinx.[ch]

--- src/tests/test.c
+++ src/tests/test.c
@@ -1,20 +1,23 @@
 #include "../sphinx.h"
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
+#include <sodium.h>
 
 int main(void) {
   const uint8_t pwd[]="shitty password";
-  const uint8_t secret[SPHINX_255_SCALAR_BYTES]="                                ";
+  const uint8_t secret[SPHINX_255_SCALAR_BYTES]={1};
+  const uint8_t salt[crypto_pwhash_SALTBYTES]={1};
   uint8_t bfac[SPHINX_255_SCALAR_BYTES],
     chal[SPHINX_255_SER_BYTES],
     resp[SPHINX_255_SER_BYTES],
     rwd[SPHINX_255_SER_BYTES];
 
-  sphinx_challenge(pwd, sizeof pwd, bfac, chal);
+  sphinx_challenge(pwd, strlen((char*) pwd), salt, crypto_pwhash_SALTBYTES, bfac, chal);
   if(0!=sphinx_respond(chal, secret, resp)) {
     return 1;
   }
-  if(0!=sphinx_finish(pwd, sizeof pwd, bfac, resp, rwd)) {
+  if(0!=sphinx_finish(pwd, sizeof pwd, bfac, resp, salt, rwd)) {
     return 1;
   }
 
