7a50499dd6f32d1ef33520beeac129eb118abe14
stf <7o5rfu92t@ctrlc.hu>
2022-06-08 18:43:34 +0000

    [mod] fixed license headers

--- src/common.h
+++ src/common.h
@@ -1,5 +1,22 @@
 #ifndef COMMON_H
 #define COMMON_H
+/*
+    @copyright 2018-2021, sphinx@ctrlc.hu
+    This file is part of libsphinx.
+
+    libsphinx is free software: you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public License
+    as published by the Free Software Foundation, either version 3 of
+    the License, or (at your option) any later version.
+
+    libsphinx is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with libsphinx. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <stdint.h>
 #include <sodium.h>
